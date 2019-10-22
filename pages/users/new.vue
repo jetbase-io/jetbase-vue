3dca6a562d85efe61ba9f1ff346fd902ef9e3e1f
lukas <kosha.industry@gmail.com>
2019-10-23 01:05:04 +0600

    after user created redirect to back

--- pages/users/new.vue
+++ pages/users/new.vue
@@ -17,7 +17,7 @@
       <transition name="fade" appear>
         <div class="row">
           <div class="col-lg-6">
-            <user-card-form />
+            <user-card-form @created="$router.back()" />
           </div>
         </div>
       </transition>
