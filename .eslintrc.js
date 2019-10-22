344e5084e1f91c3dd88c9c98a853eece1a306909
lukas <kosha.industry@gmail.com>
2019-10-23 00:28:54 +0600

    update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,13 @@ module.exports = {
   // add your custom rules here
   rules: {
     'no-console': 'off',
-    'require-await': 'warn'
+    'require-await': 'warn',
+
+    // @see https://bit.ly/31J1oes
+    "vue/singleline-html-element-content-newline": ["warn", {
+      "ignoreWhenNoAttributes": true,
+      "ignoreWhenEmpty": true,
+      "ignores": ['pre', 'textarea', 'template', 'nuxt-link']
+    }]
   }
 }
