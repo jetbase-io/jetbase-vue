f8aa73c65486cd6fddb87668d40bb437f4091e9e
lukas <kosha.industry@gmail.com>
2019-10-22 20:19:38 +0600

    add app header to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,14 @@
 <template>
   <div id="layout-default">
+    <app-header />
     <nuxt />
   </div>
 </template>
+
+<script>
+import AppHeader from '../components/app-header'
+
+export default {
+  components: { AppHeader }
+}
+</script>
