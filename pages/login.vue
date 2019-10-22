09d01255aa173421a422e25d93684ec676fec778
lukas <kosha.industry@gmail.com>
2019-10-22 18:17:37 +0600

    add login page

---
+++ pages/login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div class="justify-content-center row">
+      <div class="col-md-8">
+        <div class="card-group">
+          <div class="p-4 card">
+            <div class="card-body">
+              <login-form />
+            </div>
+          </div>
+          <div class="text-white bg-primary py-5 d-md-down-none card" style="width: 44%;">
+            <div class="text-center card-body">
+              <div>
+                <h2>Sign up</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur</p>
+                <a href="#">
+                  <button tabindex="-1" class="mt-3 btn btn-primary active">Register Now!</button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LoginForm from '../components/login-form'
+
+export default {
+  components: { LoginForm },
+  layout: 'auth'
+}
+</script>
