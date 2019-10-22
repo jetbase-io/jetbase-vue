431bb5fc717243951368240e7bf9c64c7f65d680
lukas <kosha.industry@gmail.com>
2019-10-22 19:10:54 +0600

    update login form

--- components/login-form.vue
+++ components/login-form.vue
@@ -1,5 +1,5 @@
 <template>
-  <form>
+  <form @submit.prevent="submit">
     <h1>Login</h1>
     <p class="text-muted">
       Sign In to your account
@@ -8,13 +8,29 @@
       <div class="input-group-prepend">
         <span class="input-group-text"><i class="fas fa-user" /></span>
       </div>
-      <input id="email" placeholder="E-mail" autocomplete="email" type="text" class="form-control">
+      <label for="email" class="sr-only">E-mail</label>
+      <input
+        id="email"
+        v-model="form.email"
+        placeholder="E-mail"
+        autocomplete="email"
+        type="text"
+        class="form-control"
+      >
     </div>
     <div class="mb-4 input-group">
       <div class="input-group-prepend">
         <span class="input-group-text"><i class="fas fa-lock" /></span>
       </div>
-      <input id="password" placeholder="Password" autocomplete="current-password" type="password" class="form-control">
+      <label for="password" class="sr-only">Password</label>
+      <input
+        id="password"
+        v-model="form.password"
+        placeholder="Password"
+        autocomplete="current-password"
+        type="password"
+        class="form-control"
+      >
     </div>
     <div class="row">
       <div class="col-6">
@@ -30,3 +46,54 @@
     </div>
   </form>
 </template>
+
+<script>
+import { validationMixin } from 'vuelidate'
+import { required, email } from 'vuelidate/lib/validators'
+
+export default {
+  head: {
+    title: 'Login'
+  },
+  mixins: [validationMixin],
+  data () {
+    return {
+      form: {
+        email: null,
+        password: null
+      },
+      error: null,
+      submitting: false
+    }
+  },
+  validations: {
+    form: {
+      email: { required, email },
+      password: { required }
+    }
+  },
+  methods: {
+    async submit () {
+      this.$v.form.$touch()
+      if (this.$v.form.$invalid) {
+        return // invalid form
+      }
+
+      this.error = null
+
+      try {
+        this.submitting = true
+        await this.$auth.login({ data: this.form })
+      } catch (e) {
+        if (e.response && [400].includes(e.response.status)) {
+          this.error = 'Неверный Email или пароль'
+        } else {
+          throw e
+        }
+      } finally {
+        this.submitting = false
+      }
+    }
+  }
+}
+</script>
