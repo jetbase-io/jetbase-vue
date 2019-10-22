cc50f3ad1625d6ae290b003fa10b92dfc7859102
lukas <kosha.industry@gmail.com>
2019-10-22 20:52:28 +0600

    update index page

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,9 @@
         <i class="fas fa-tachometer-alt" /> Dashboard
       </div>
       <div class="card-body">
-        TODO some statistics and metrics badges
+        <p class="text-danger">
+          TODO some statistics and metrics badges
+        </p>
       </div>
     </div>
   </div>
