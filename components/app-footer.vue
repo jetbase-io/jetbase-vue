f39d2ce030a25e77c2a38ebdb253bf0812346a81
lukas <kosha.industry@gmail.com>
2019-10-22 21:04:13 +0600

    add app-footer.vue

---
+++ components/app-footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="app-footer">
+    <span><a href="https://jetbase.io" target="_blank">JetBase</a> © 2019.</span>
+    <span class="ml-auto">
+      Powered by <a href="https://jetbase.io" target="_blank">JetBase for Vue</a>
+    </span>
+  </footer>
+</template>
+
+<style lang="scss">
+.app-footer {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  padding: 0 1rem;
+  color: #23282c;
+  background: #f0f3f5;
+  border-top: 1px solid #c8ced3;
+  flex: 0 0 50px;
+}
+</style>
