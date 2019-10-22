671ee2d6c1f58d1772d4ae525b10113fc5cb64ff
lukas <kosha.industry@gmail.com>
2019-10-22 21:13:14 +0600

    add breadcrumb

--- pages/users.vue
+++ pages/users.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="container-fluid">
-    users
+  <div>
+    <!--breadcrumb-->
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <nuxt-link :to="{name:'index'}">
+            Dashboard
+          </nuxt-link>
+        </li>
+        <li class="active breadcrumb-item" aria-current="page">
+          Users
+        </li>
+      </ol>
+    </nav>
+
+    <!--page content-->
+    <div class="container-fluid">
+      users
+    </div>
   </div>
 </template>
