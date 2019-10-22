e93049ab8a29a68a8ddc2da42e8b065232d20a4c
lukas <kosha.industry@gmail.com>
2019-10-22 21:24:12 +0600

    add users table

---
+++ components/users-table-row.vue
@@ -0,0 +1,50 @@
+<template>
+  <tr>
+    <th scope="row">
+      <nuxt-link :to="userRoute">
+        {{ user.id }}
+      </nuxt-link>
+    </th>
+    <td>
+      <nuxt-link :to="userRoute">
+        {{ user.username }}
+      </nuxt-link>
+    </td>
+    <td>
+      <nuxt-link :to="userRoute">
+        {{ user.first_name }}
+      </nuxt-link>
+    </td>
+    <td>
+      <nuxt-link :to="userRoute">
+        {{ user.last_name }}
+      </nuxt-link>
+    </td>
+    <td>
+      {{ user.email }}
+    </td>
+    <td>
+      <!--todo role-->
+      <span class="badge badge-success">role</span>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    userRoute () {
+      return {
+        name: 'users-id',
+        params: { id: this.user.id }
+      }
+    }
+  }
+}
+</script>
---
+++ components/users-table.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="table-responsive">
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">
+            id
+          </th>
+          <th scope="col">
+            username
+          </th>
+          <th scope="col">
+            first name
+          </th>
+          <th scope="col">
+            last name
+          </th>
+          <th scope="col">
+            email
+          </th>
+          <th scope="col">
+            status
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <users-table-row v-for="user in users" :key="user.id" :user="user" />
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import UsersTableRow from './users-table-row'
+
+export default {
+  components: { UsersTableRow },
+  props: {
+    users: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
