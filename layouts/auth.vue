1c74e7ec8cf5b5761af5ef3a01622224be5fe89d
lukas <kosha.industry@gmail.com>
2019-10-22 18:16:28 +0600

    add auth layout

---
+++ layouts/auth.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="layout-auth" class="d-flex flex-row align-items-center">
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    bodyAttrs: {
+      class: 'bg-light'
+    }
+  }
+}
+</script>
