eebbb396d51783f274947639d38d2ed6cd31e6cf
lukas <kosha.industry@gmail.com>
2019-10-23 20:17:01 +0600

    change test name

--- test/AppFooter.spec.js
+++ test/AppFooter.spec.js
@@ -1,7 +1,7 @@
 import { mount } from '@vue/test-utils'
 import AppFooter from '../components/app-footer.vue'
 
-describe('Logo', () => {
+describe('AppFooter', () => {
   test('is a Vue instance', () => {
     const wrapper = mount(AppFooter)
     expect(wrapper.isVueInstance()).toBeTruthy()
