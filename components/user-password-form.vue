3dfe83ded66c50ba3e1071f97b2c21969ad3ca02
lukas <kosha.industry@gmail.com>
2019-10-23 13:38:18 +0600

    add user-password-form.vue

---
+++ components/user-password-form.vue
@@ -0,0 +1,171 @@
+<!--suppress HtmlFormInputWithoutLabel -->
+<template>
+  <div class="card mb-3">
+    <form class="user-form" @submit.prevent="submit">
+      <div class="card-header">
+        <i class="fas fa-key text-muted" />Change User Password: {{ user.id }}
+      </div>
+      <div class="card-body">
+
+        <!--old password-->
+        <div class="form-group">
+          <label :for="inputPrefix('password_old')">Old password</label>
+          <password-wrap :visible.sync="showPassword">
+            <input
+              :id="inputPrefix('password_old')"
+              v-model="form.password_old"
+              :type="showPassword ? 'text' : 'password'"
+              :placeholder="`Enter old password`"
+              class="form-control"
+              :class="{'is-invalid': $v.form.password_old.$error}"
+              autocomplete="off"
+            >
+            <div v-if="$v.form.password_old.$error" class="invalid-feedback">
+              <template v-if="!$v.form.password_old.required">Please enter old password</template>
+            </div>
+          </password-wrap>
+        </div>
+
+        <!--password-->
+        <div class="form-group">
+          <label :for="inputPrefix('password')">Password</label>
+          <!--todo generate link-->
+          <password-wrap :visible.sync="showPassword">
+            <input
+              :id="inputPrefix('password')"
+              v-model="form.password"
+              :type="showPassword ? 'text' : 'password'"
+              :placeholder="`Enter password, min ${passwordMinLength} symbols`"
+              class="form-control"
+              :class="{'is-invalid': $v.form.password.$error}"
+              autocomplete="off"
+            >
+            <div v-if="$v.form.password.$error" class="invalid-feedback">
+              <template v-if="!$v.form.password.required">Please enter password</template>
+              <template v-if="!$v.form.password.minLength">Password minimum length {{ passwordMinLength }} symbols</template>
+            </div>
+          </password-wrap>
+        </div>
+
+        <!--password confirmation-->
+        <div class="form-group">
+          <label :for="inputPrefix('password_confirmation')">Confirm Password</label>
+          <password-wrap :visible.sync="showPassword">
+            <input
+              :id="inputPrefix('password_confirmation')"
+              v-model="form.password_confirmation"
+              :type="showPassword ? 'text' : 'password'"
+              :placeholder="`Repeat password again`"
+              class="form-control"
+              :class="{'is-invalid': $v.form.password_confirmation.$error}"
+              autocomplete="off"
+            >
+            <div v-if="$v.form.password_confirmation.$error" class="invalid-feedback">
+              <template v-if="!$v.form.password_confirmation.required">Please repeat password</template>
+              <template v-if="!$v.form.password_confirmation.sameAsPassword">Passwords are different</template>
+            </div>
+          </password-wrap>
+        </div>
+      </div>
+      <div class="card-footer">
+        <button type="submit" class="btn btn-primary btn-sm" :disabled="submitting">
+          <i :class="submitting ? 'fas fa-spinner fa-spin' : 'far fa-dot-circle'" />
+          Change Password
+        </button>
+        <a href="/users" class="btn btn-danger btn-sm" @click.prevent="cancel">
+          <i class="fas fa-ban" /> Cancel
+        </a>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { validationMixin } from 'vuelidate'
+import { required, minLength, sameAs } from 'vuelidate/lib/validators'
+import { CancelToken } from 'axios'
+import { PASSWORD_MIN_LENGTH } from '../config'
+import PasswordWrap from './password-wrap'
+
+export default {
+  components: { PasswordWrap },
+  mixins: [validationMixin],
+  props: {
+    user: {
+      type: Object,
+      required: true
+    },
+    inputIdPrefix: {
+      type: String,
+      default: 'user-password-form-'
+    },
+    passwordMinLength: {
+      type: Number,
+      default: PASSWORD_MIN_LENGTH
+    }
+  },
+  data () {
+    return {
+      form: {
+        password_old: null,
+        password: null,
+        password_confirmation: null
+      },
+      showPassword: false,
+      submitting: false,
+      cancelXhr: null
+    }
+  },
+  validations () {
+    return {
+      form: {
+        password_old: { required },
+        password: { required, minLength: minLength(this.passwordMinLength) },
+        password_confirmation: { required, sameAsPassword: sameAs('password') }
+      }
+    }
+  },
+  methods: {
+    inputPrefix (id) {
+      return this.inputIdPrefix + id
+    },
+    async submit () {
+      // client validation
+      this.$v.form.$touch()
+      if (this.$v.form.$invalid) {
+        return // invalid form
+      }
+
+      try {
+        this.submitting = true
+        // change user's password
+        const uri = 'users/' + this.user.id + '/password'
+        const res = await this.$api.put(uri, this.form, {}, new CancelToken((cancel) => {
+          this.cancelXhr = cancel // uses for cancel xhr
+        }))
+        this.cancelXhr = null
+        this.$toasted.success('Password successfully changed')
+        this.$emit('changed', res)
+      } catch (e) {
+        let errorHandled = false
+
+        // handle server validation errors
+        if (e.response && [400].includes(e.response.status)) {
+          const message = e.response.data.message || e.response.statusText || 'Server Error'
+          alert(message)
+          errorHandled = true
+        }
+        if (!errorHandled) {
+          throw e
+        }
+      } finally {
+        this.submitting = false
+      }
+    },
+    cancel () {
+      this.cancelXhr && this.cancelXhr()
+      this.$router.back()
+    }
+  }
+}
+</script>
--- pages/users/_id/password.vue
+++ pages/users/_id/password.vue
@@ -1,6 +1,36 @@
 <template>
   <div>
-    <!--todo stop here: change password form-->
-    change password form
+    <!--breadcrumb-->
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <nuxt-link :to="{name:'index'}">Dashboard</nuxt-link>
+        </li>
+        <li class="breadcrumb-item">
+          <nuxt-link :to="{name:'users'}">Users</nuxt-link>
+        </li>
+        <li class="active breadcrumb-item" aria-current="page">Change Password</li>
+      </ol>
+    </nav>
+
+    <div class="container-fluid">
+      <transition name="fade" appear>
+        <div class="row">
+          <div class="col-lg-6">
+            <password-form :user="user" @changed="$router.back()" />
+          </div>
+        </div>
+      </transition>
+    </div>
   </div>
 </template>
+
+<script>
+import PasswordForm from '../../../components/user-password-form'
+import UserIdPage from '../../../components/user-id-page'
+
+export default {
+  components: { PasswordForm },
+  extends: UserIdPage
+}
+</script>
